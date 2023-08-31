5001cfe65539ffcef33ff38b3b76a41bb1aa395a
PerdomoPer <gustavo.perdomo@estudante.ifms.edu.br>
2023-08-31 08:26:18 -0400

    barbie

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,13 +1,19 @@
+import Imagem from "./../../assets/filme.png"
 import './Filme.css'
-import imagem from ".././../assets/bb.jpg"
-
-function Filme(){
+type FilmeProps = {
+    titulo:string,
+    sinopse:string,
+    imagem:string
+}
+function Filme(props:FilmeProps){
     return(
         <div className="filme-content">
-        <img src={imagem} alt="" /> 
-
+            <img className="img-filme" src={Imagem} alt="" />
+            <div className="text-barbie">
+                <h1>{props.titulo}</h1>
+                <p className="sinopse">{props.sinopse}</p>
+            </div>
         </div>
-        
     )
 }
 export default Filme
