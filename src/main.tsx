60104a16ae38ed3994df54ae36ae071a62c32ca6
PerdomoPer <gustavo.perdomo@estudante.ifms.edu.br>
2023-09-28 08:26:19 -0400

    aula28/09

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,38 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
+import "./main.css"
+import{createBrowserRouter, RouterProvider} from "react-router-dom"
+import Header from './componentes/header/Header.tsx';
+import Main from './componentes/main/Main.tsx';
+import Footer from './componentes/footer/Footer.tsx';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <> 
+    <Header/>
+    <Main/>
+    <Footer/>
+    </>,
+  },
+  {
+    path: "/noticias",
+    element: <div>Pagina de noticias</div>,
+  },
+  {
+    path: "/sobre",
+    element: <div>Pagina Sobre</div>,
+  },
+  {
+    path: "/contato",
+    element: <div>Pagina de Contado</div>,
+  },
+]);
+
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router = {router}/>
   </React.StrictMode>,
 )
